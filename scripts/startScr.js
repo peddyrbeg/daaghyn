6e333e3f337a0f63f2cc902dde7c47b83279d072
peddyrbeg <85888409+peddyrbeg@users.noreply.github.com>
2022-01-13 15:19:51 +0000

    Add files via upload

---
+++ scripts/startScr.js
@@ -0,0 +1,26 @@
+class startPic {
+
+	constructor () {
+		this.c = Math.floor(random(0,7));
+		this.daah = daah[this.c];
+		this.x = Math.floor(random(0, 220));
+		this.y = Math.floor(random(0, height));
+		this.r = random(0, 0.5);
+		this.s = random(50, 150);
+		this.speed = random(1, 2);
+	}
+
+	display () {
+		push();
+		if (this.x > 420) {
+			this.c = Math.floor(random(0,7));
+			this.x = -150;
+			this.y = Math.floor(random(0, height));
+		}
+		translate(this.x += this.speed, this.y--);
+		rotate(this.r);
+		image(this.daah, 0, 0, this.s, this.s);
+		pop();
+	}
+
+}
